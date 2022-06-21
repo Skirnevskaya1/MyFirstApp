f04efd11dd1adba43b57519b9227a974bf244b59
Kate <57950489+Skirnevskaya1@users.noreply.github.com>
2022-06-22 00:56:39 +0700

    lesson1

---
+++ app/src/main/java/com/android/myfirstapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.android.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+    /*
+    1. Создать проект со следующими пользовательскими элементами: TextView, EditText, Button, Switch, CheckBox, ToggleButton. Для работы использовать LinearLayout. Использовать различные шрифты, цвета, размеры, прочие атрибуты.
+    2. Создать ещё один макет (если не получается, то проект) с несколькими EditText, где использовать атрибут inputType: text, textPersonName, phone, number, textPassword, textEmailAddress и другие значения.
+    3. Добавить в проект элемент CalendarView.
+    4. * Разобраться, что такое параметр ems.
+     */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
